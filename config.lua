128e921c2d8d14b7129a9846296b067326421d6d
DJultra14 <callumglasspool@gmail.com>
2026-03-30 19:13:24 +0100

    added default outfit

--- config.lua
+++ config.lua
@@ -14,26 +14,32 @@ Config.EnableStaffText = true -- Puts text above on duty staff members heads
 Config.Clothing = { -- Clothing to swap into for each group
     ["god"] = {
         components = {
-            { component_id = 3, drawable = 32, texture = 0, palette = 0 },
-            { component_id = 4, drawable = 321, texture = 0, palette = 0 },
-            { component_id = 6, drawable = 32, texture = 0, palette = 0 },
-            { component_id = 11, drawable = 10, texture = 0, palette = 0 },
+            { component_id = 1, drawable = 129, texture = 0, palette = 0 }, -- mask
+            { component_id = 3, drawable = 204, texture = 0, palette = 0 }, -- arms
+            { component_id = 4, drawable = 197, texture = 0, palette = 0 }, -- pants
+            { component_id = 6, drawable = 110, texture = 0, palette = 0 }, -- shoes
+            { component_id = 8, drawable = 15, texture = 0, palette = 0 }, -- undershirt
+            { component_id = 11, drawable = 533, texture = 0, palette = 0 }, -- jacket / torso 2
         },
     },
     ["admin"] = {
         components = {
-            { component_id = 3, drawable = 29, texture = 0, palette = 0 },
-            { component_id = 4, drawable = 15, texture = 0, palette = 0 },
-            { component_id = 6, drawable = 28, texture = 0, palette = 0 },
-            { component_id = 11, drawable = 250, texture = 0, palette = 0 },
+            { component_id = 1, drawable = 129, texture = 0, palette = 0 }, -- mask
+            { component_id = 3, drawable = 204, texture = 0, palette = 0 }, -- arms
+            { component_id = 4, drawable = 197, texture = 0, palette = 0 }, -- pants
+            { component_id = 6, drawable = 110, texture = 0, palette = 0 }, -- shoes
+            { component_id = 8, drawable = 15, texture = 0, palette = 0 }, -- undershirt
+            { component_id = 11, drawable = 533, texture = 0, palette = 0 }, -- jacket / torso 2
         },
     },
     ["mod"] = {
         components = {
-            { component_id = 3, drawable = 44, texture = 0, palette = 0 },
-            { component_id = 4, drawable = 15, texture = 0, palette = 0 },
-            { component_id = 6, drawable = 28, texture = 0, palette = 0 },
-            { component_id = 11, drawable = 250, texture = 0, palette = 0 },
+            { component_id = 1, drawable = 129, texture = 0, palette = 0 }, -- mask
+            { component_id = 3, drawable = 204, texture = 0, palette = 0 }, -- arms
+            { component_id = 4, drawable = 197, texture = 0, palette = 0 }, -- pants
+            { component_id = 6, drawable = 110, texture = 0, palette = 0 }, -- shoes
+            { component_id = 8, drawable = 15, texture = 0, palette = 0 }, -- undershirt
+            { component_id = 11, drawable = 533, texture = 0, palette = 0 }, -- jacket / torso 2
         },
     },
 
